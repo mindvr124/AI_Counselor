d784d5031c901043a7473bc32adc05b2953d2543
mindvr124 <mindvr124@gmail.com>
2025-07-15 14:29:48 +0900

    edit  asgi.py

--- server/asgi.py
+++ server/asgi.py
@@ -1,7 +1,9 @@
 import os
 from django.core.asgi import get_asgi_application
 from server.routing import application as websocket_application
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import aicounselor.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
 
@@ -9,5 +11,9 @@ django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    "websocket": websocket_application,
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            aicounselor.routing.websocket_urlpatterns  # URL 패턴 연결
+        )
+    ),
 })
